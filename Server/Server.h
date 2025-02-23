8f03e116d05f4965fac3530c6ca1e0cb5098918d
Sidminik <okb.dns@gmail.com>
2025-02-23 07:52:37 +0700

    Update Server.h

--- Server/Server.h
+++ Server/Server.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define _WINSOCK_DEPRECATED_NO_WARNINGS     
+#define _WINSOCK_DEPRECATED_NO_WARNINGS     // макрос для отключения устаревших предупреждений API
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -10,30 +10,30 @@
 #include "User.h"
 #include "Contact.h"
 
-#pragma comment(lib, "WS2_32.lib")          
+#pragma comment(lib, "WS2_32.lib")          // стандартная библиотека импорта
 
 class Server
 {
     SOCKET serverSocket;                    // сокет сервера
     SOCKADDR_IN serverAddr;                 // адрес сокета сервера
-    std::vector<User> usersConnections;     // вектор хранения подключений пользователя (клиентов)
+    std::vector<std::shared_ptr<User>> usersConnections;     // вектор хранения подключений пользователя (клиентов)
     int allUsersCounter;                    // счетчик всех клиентов
     int connectedUsersCounter;              // счетчик подключенных клиентов
-    std::recursive_mutex mtxNewConnect;     // синхронизация в методе serverReceive
-    std::recursive_mutex mtxDisconnect;     // синхронизация в методе socketError
-    std::recursive_mutex mtxNewMessage;     // синхронизация в методе sendCommandProcessing
+    std::recursive_mutex mtxNewConnect;     // синхронизация критической секции внутри serverReceive
+    std::recursive_mutex mtxDisconnect;     // синхронизация критической секции внутри socketError
+    std::recursive_mutex mtxNewMessage;     // синхронизация критической секции внутри sendCommandProcessing
 public:
     Server();
     ~Server();
 
     void createNewClient();                                                         // создание потока нового клиента
     void serverReceive(SOCKET transmittedSocket);                                   // подключение нового клиента
-    void socketError(User currentUser);                                             // обработка ошибки сокета
-    void exitProcessing(User currentUser);                                          // обработка команды /exit
-    void startProcessing(User currentUser);                                         // обработка команды /start
-    void clientCountProcessing(User currentUser) const;                             // обработка команды /client_count
-    void sendCommandProcessing(User currentUser, std::string sendCommandString);    // обработка команды /send_message
-    void showCommandProcessing(User currentUser, std::string showCommandString);    // обработка команды /show_message 
+    void socketError(std::shared_ptr<User> currentUser);                                             // обработка ошибки сокета
+    void exitProcessing(std::shared_ptr<User> currentUser);                                          // обработка команды /exit
+    void startProcessing(std::shared_ptr<User> currentUser);                                         // обработка команды /start
+    void clientCountProcessing(std::shared_ptr<User> currentUser) const;                             // обработка команды /client_count
+    void sendCommandProcessing(std::shared_ptr<User> currentUser, std::string sendCommandString);    // обработка команды /send_message
+    void showCommandProcessing(std::shared_ptr<User> currentUser, std::string showCommandString);    // обработка команды /show_message 
 };
 
 Server::Server()
@@ -65,7 +65,7 @@ Server::Server()
 Server::~Server()
 {
     closesocket(serverSocket);
-    WSACleanup();
+    WSACleanup(); // Освобождаем ресурсы WinSock
 };
 
 void Server::createNewClient()
@@ -85,15 +85,14 @@ void Server::createNewClient()
     }
 }
 
-
 void Server::serverReceive(SOCKET transmittedSocket)
 {
     mtxNewConnect.lock();
-        allUsersCounter++;
-        User currentUser(transmittedSocket, allUsersCounter);
-        usersConnections.push_back(currentUser);
-        connectedUsersCounter++;
-        std::cout << "Клиент " << currentUser.getNumber() << " подключен!" << std::endl;
+    allUsersCounter++;
+    std::shared_ptr<User> currentUser = std::make_shared<User>(transmittedSocket, allUsersCounter);
+    usersConnections.push_back(currentUser);
+    connectedUsersCounter++;
+    std::cout << "Клиент " << currentUser -> getNumber() << " подключен!" << std::endl;
     mtxNewConnect.unlock();
 
     char buffer[1024] = { 0 };
@@ -102,7 +101,7 @@ void Server::serverReceive(SOCKET transmittedSocket)
 
     while (true) // цикл работы сервера
     {
-        if (recv(currentUser.getSocket(), buffer, sizeof(buffer), 0) == SOCKET_ERROR)
+        if (recv(currentUser -> getSocket(), buffer, sizeof(buffer), 0) == SOCKET_ERROR)
         {
             socketError(currentUser);
             return;
@@ -133,22 +132,22 @@ void Server::serverReceive(SOCKET transmittedSocket)
         }
         else
         {
-            std::cout << "Клиент " << currentUser.getNumber() << ": " << buffer;
+            std::cout << "Клиент " << currentUser -> getNumber() << ": " << buffer;
         }
         memset(buffer, 0, sizeof(buffer));
     }
 }
 
-void Server::socketError(User currentUser)
+void Server::socketError(std::shared_ptr<User> currentUser)
 {
     std::lock_guard<std::recursive_mutex> lock(mtxDisconnect);
 
-    std::cerr << "Соединение с клиентом " << currentUser.getNumber() << " завершилось сбоем с ошибкой : "
+    std::cerr << "Соединение с клиентом " << currentUser -> getNumber() << " завершилось сбоем с ошибкой : "
         << WSAGetLastError() << std::endl;
 
     for (int i = 0; i < usersConnections.size(); i++)
     {
-        if (usersConnections[i].getNumber() == currentUser.getNumber())
+        if (usersConnections[i] -> getNumber() == currentUser -> getNumber())
         {
             usersConnections.erase(usersConnections.begin() + i);
         }
@@ -156,61 +155,61 @@ void Server::socketError(User currentUser)
     connectedUsersCounter--;
 }
 
-void Server::exitProcessing(User currentUser)
+void Server::exitProcessing(std::shared_ptr<User> currentUser)
 {
-    std::cout << "Клиент " << currentUser.getNumber() << " отсоединился." << std::endl;
+    std::cout << "Клиент " << currentUser -> getNumber() << " отсоединился." << std::endl;
     connectedUsersCounter--;
     std::string localMessage = { "Произошло отключение от сервера\n" };
-    send(currentUser.getSocket(), localMessage.c_str(), (int) localMessage.length(), 0);
+    send(currentUser -> getSocket(), localMessage.c_str(), (int)localMessage.length(), 0);
 }
 
-void Server::startProcessing(User currentUser)
+void Server::startProcessing(std::shared_ptr<User> currentUser)
 {
-    std::string localMessage = { "Привет. Ваш порядковый номер - " + std::to_string(currentUser.getNumber()) + "\n"};
-    send(currentUser.getSocket(), localMessage.c_str(), (int)localMessage.length(), 0);
+    std::string localMessage = { "Привет. Ваш порядковый номер - " + std::to_string(currentUser -> getNumber()) + "\n" };
+    send(currentUser -> getSocket(), localMessage.c_str(), (int)localMessage.length(), 0);
     std::cout << localMessage;
 }
 
-void Server::clientCountProcessing(User currentUser) const
+void Server::clientCountProcessing(std::shared_ptr<User> currentUser) const
 {
-    std::string localMessage = { "Количество клиентов, подключенных сейчас - " 
+    std::string localMessage = { "Количество клиентов, подключенных сейчас - "
         + std::to_string(connectedUsersCounter) + "\n" };
-    send(currentUser.getSocket(), localMessage.c_str(), (int) localMessage.length(), 0);
+    send(currentUser -> getSocket(), localMessage.c_str(), (int)localMessage.length(), 0);
     std::cout << localMessage;
 }
 
-void Server::sendCommandProcessing(User currentUser, std::string sendCommandString)
+void Server::sendCommandProcessing(std::shared_ptr<User> currentUser, std::string sendCommandString)
 {
     std::lock_guard<std::recursive_mutex> lock(mtxNewMessage);
 
     std::string command = "/send_message_";
-    int senderNumber = currentUser.getNumber();
+    int senderNumber = currentUser -> getNumber();
     int recipientNumber = stoi(sendCommandString.substr(command.length(), sendCommandString.length()));
-    int endCommandIndex = (int) sendCommandString.find(":");
+    int endCommandIndex = (int)sendCommandString.find(":");
 
     std::string localMessageShort = sendCommandString.substr(endCommandIndex + 1, sendCommandString.length());
     std::string localMessageLong = "Текст нового сообщения от клиента " + std::to_string(senderNumber) + ": "
         + localMessageShort + "\n";
 
     std::cout << "Текст сообщения для клиента " << recipientNumber << ": " << localMessageShort;
-    
+
     for (int i = 0; i < usersConnections.size(); i++)
     {
-        if (usersConnections[i].getNumber() == recipientNumber)
+        if (usersConnections[i] -> getNumber() == recipientNumber)
         {
-            usersConnections[recipientNumber - 1].addMessage(senderNumber, localMessageShort);
+            usersConnections[recipientNumber - 1] -> addMessage(senderNumber, localMessageShort);
 
-            if (send(usersConnections[recipientNumber - 1].getSocket(), localMessageLong.c_str(),
+            if (send(usersConnections[recipientNumber - 1] -> getSocket(), localMessageLong.c_str(),
                 (int)localMessageLong.length() - 1, 0) == SOCKET_ERROR)
             {
                 std::string errorMessage = "Cообщение отправлено с ошибкой\n";
-                send(currentUser.getSocket(), errorMessage.c_str(), (int)errorMessage.length(), 0);
+                send(currentUser -> getSocket(), errorMessage.c_str(), (int)errorMessage.length(), 0);
                 std::cout << errorMessage;
             }
             else
             {
                 std::string successfulMessage = "Cообщение отправлено успешно\n";
-                send(currentUser.getSocket(), successfulMessage.c_str(), (int)successfulMessage.length(), 0);
+                send(currentUser -> getSocket(), successfulMessage.c_str(), (int)successfulMessage.length(), 0);
                 std::cout << successfulMessage;
             }
             return;
@@ -218,34 +217,34 @@ void Server::sendCommandProcessing(User currentUser, std::string sendCommandStri
         else
         {
             std::string errorUser = "Отсутствует получатель с указанным номером\n";
-            send(currentUser.getSocket(), errorUser.c_str(), (int)errorUser.length(), 0);
+            send(currentUser -> getSocket(), errorUser.c_str(), (int)errorUser.length(), 0);
             std::cout << errorUser;
             return;
         }
     }
 }
 
-void Server::showCommandProcessing(User currentUser, std::string showCommandString)
+void Server::showCommandProcessing(std::shared_ptr<User> currentUser, std::string showCommandString)
 {
     std::string commandPart1 = "/show_message_";
     std::string commandPart2 = "_from_client_";
 
-    int indexPart2 = (int) showCommandString.find("_from_client_");
+    int indexPart2 = (int)showCommandString.find("_from_client_");
     int messageNumber = stoi(showCommandString.substr(commandPart1.length(), indexPart2));
 
     if ((indexPart2 + commandPart2.length()) < (showCommandString.length() - 1))
     {
-        int recipientNumber = currentUser.getNumber();
+        int recipientNumber = currentUser -> getNumber();
         int senderNumber = stoi(showCommandString.substr(
             indexPart2 + commandPart2.length(), showCommandString.length()));
 
         for (int i = 0; i < usersConnections.size(); i++)
         {
-            if (usersConnections[i].getNumber() == recipientNumber)
+            if (usersConnections[i] -> getNumber() == recipientNumber)
             {
-                std::string localMessageShort = usersConnections[i].getMessage(senderNumber, messageNumber);
+                std::string localMessageShort = usersConnections[i] -> getMessage(senderNumber, messageNumber);
                 std::string localMessageLong = localMessageShort + "\n";
-                send(currentUser.getSocket(), localMessageLong.c_str(), (int)localMessageLong.length() - 1, 0);
+                send(currentUser -> getSocket(), localMessageLong.c_str(), (int)localMessageLong.length() - 1, 0);
                 std::cout << localMessageShort;
                 break;
             }
